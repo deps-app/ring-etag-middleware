e6fb2a663efc7b01a04c43d8b5397358d2e81cef
Daniel Compton <desk@danielcompton.net>
2018-03-20 21:16:13 +1300

    Set metadata for projecdt.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject co.deps/ring-etag-middleware "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ring middleware to set an ETag on responses"
+  :url "https://github.com/deps-app/ring-etag-middleware"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :global-vars {*warn-on-reflection* true}
