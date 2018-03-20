7c31d51b2cd46690b29368f8c91687958c8e4aac
Daniel Compton <desk@danielcompton.net>
2018-03-20 21:18:08 +1300

    Add deploy repositories

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :global-vars {*warn-on-reflection* true}
+  :deploy-repositories {"releases" :clojars
+                        "snapshots" :clojars}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.6.3"]])
